1c7b3a1748132ad8716ba9e16f4f75c5436e3eac
Hajar <hajar.choukri004@gmail.com>
2026-03-26 09:47:15 +0100

    Add SQL queries extracted from notebooks

---
+++ queries/review_analysis_queries.sql
@@ -0,0 +1,3 @@
+-- Queries from Review Analysis Notebook --
+-- Created on: Thu Mar 26 09:46:17 AM +01 2026
+
---
+++ queries/review_manipulation_queries.sql
@@ -0,0 +1,3 @@
+-- Queries from Review Manipulation Detection Notebook --
+-- Created on: Thu Mar 26 09:46:17 AM +01 2026
+
